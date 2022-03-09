cd30bc0eaa2332399fb2c74af768b7a7e5f2e663
tigerob <obrienxyz@gmail.com>
2022-03-09 16:40:26 +1100

    add bootstrap alerts messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    def assign_alert_class(flash_type)
+        case flash_type
+        when "notice"
+            return "success"
+        when "alert"
+            return "warning"
+        else
+            return "info"
+        end
+    end
+
 end
