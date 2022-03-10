9cfe69effd6ad2e1c048121f8ac4047efd500fa7
tigerob <obrienxyz@gmail.com>
2022-03-11 00:15:55 +1100

    add helpers to format price and condition in views/listings/show.html.erb

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,8 @@
 module ListingsHelper
+    def format_price(price)
+        "$#{price/100}"
+    end
+    def format_condition(condition)
+        condition.gsub("_", " - ").capitalize
+    end
 end
