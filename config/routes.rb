80c83953360c8bd4151506660f8843c24218ab31
tigerob <obrienxyz@gmail.com>
2022-03-10 19:10:43 +1100

    generate Listings view & add test listings data in seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  devise_for :users
+    
   root to: "pages#home"
+  get "/listings", to: "listings#index"
+  get "listings/show", to, "listings#show"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = ["kitchen fridge", "bar fridge", "stand-alone freezer"]
+categories = ["Kitchen fridge", "Bar fridge", "Stand-alone freezer"]
 
 if Category.count == 0
     categories.each do |category|
         Category.create(name: category)
         puts "created #{category} category"
     end
+end
+
+# create test data
+if Listing.count == 0
+    Listing.create(title: "Samsung 427L Bottom-mount Fridge", description: "Approximately four years old. I am selling because I am moving house. Perfect working order. Some superficial scratches and marks (see photos).", price: 15000, condition: 3, postcode: 3101, brand: "Samsung", model: "SRL456LS", finish: "Stainless Steel Facia", capacity: 427, height: 1700, width: 700, depth: 705, energy_efficiency: 4, user_id: 1, category_id: 1)
+    puts "created a test listing"
 end
